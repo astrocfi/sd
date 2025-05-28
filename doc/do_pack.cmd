beeabbd36c392a9ef5b2315e02f5fcc414c02275
Robert French <rfrench@seti.org>
2025-05-28 10:35:07 -0700

    Version v34.08c

--- doc/do_pack.cmd
+++ doc/do_pack.cmd
@@ -1,2 +1,2 @@
 del pack.zip
-"C:\program files\winzip\wzzip" pack *.html *.pdf *.ps *.txt *.tar.gz pdfdoc.zip psdoc.zip textdoc.zip install.exe install3.exe
+"C:\program files\winzip\wzzip" pack *.html *.pdf *.ps *.txt *.tar.gz getacro.gif pdfdoc.zip psdoc.zip textdoc.zip install.exe install3.exe
