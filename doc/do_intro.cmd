c5194d5deecc98090d560a6fa150307ff65366e0
Robert French <rfrench@seti.org>
2025-05-28 10:34:58 -0700

    Version v32.09

--- doc/do_intro.cmd
+++ doc/do_intro.cmd
@@ -1,5 +1,7 @@
 REM This uses intro.html as a source file.
+del int.msg int.txt int.zip
 echo cd public_html/sd> int.msg
 call make_text intro.html int.msg
-pgp -s +clearsig=off +armor=on +armorlines=0 int.msg -u wba
-zip int int.asc
+pgp -sta +clearsig=on +armor=on int.msg -u wba -o int.txt
+del int.msg
+REM zip int int.txt
--- doc/new_binary.cmd
+++ doc/new_binary.cmd
@@ -1,5 +1,32 @@
+REM        This creates the program executable, both for
+REM           uploading to the web and for distribution
+REM           on disk.
+REM
+REM        You must first do an appropriate MSVC++ build
+REM           in directory \wba\sd .
+REM
+REM This uses the files sd.exe, sdtty.exe, mkcalls.exe, sdlib.dll,
+REM         sd_calls.txt, and sd_calls.dat, readme,
+REM         SD.lnk, edit sd.ini.lnk, sd.ico, sdtty.ico,
+REM         sdry.ico, sdby.ico,
+REM         sample1.ini, sample2.ini, sample3.ini.
+REM
 del install.exe
 del install.zip
 "C:\program files\winzip\wzzip" install @zipfiles.lst
 "C:\program files\winzip self-extractor\wzipse32" install.zip @zipsecmds.lst
 del install.zip
+del bin.txt bin.zip
+echo cd sd> bin.msg
+echo uufile>> bin.msg
+uuencode install.exe|uufix>> bin.msg
+pgp -sta +clearsig=on +armor=on bin.msg -u wba -o bin.txt
+del bin.msg
+REM zip bin bin.txt
+REM
+REM         The file "bin.zip" may now be unzipped to "bin.txt",
+REM            which can then be mailed, creating these files:
+REM
+REM                  sd/install.exe
+REM
+REM         The file "install.exe" may now be copied to diskettes.
