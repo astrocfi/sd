1d8da73dc346ad801556dc84083cf45f418667ab
Robert French <rfrench@seti.org>
2025-05-28 10:35:22 -0700

    Version v37.83

--- doc/do_plain.cmd
+++ doc/do_plain.cmd
@@ -1,20 +1,20 @@
 mingw32-make text.all
 del textdoc.zip textdoc.tar textdoc.tar.gz
 "C:\program files\winzip\wzzip" textdoc sd_doc.txt demo.txt sessions.txt relnotes.txt relnotes98.txt relnotes03.txt appnote1.txt appnote2.txt appnote3.txt appnote4.txt oldnotes
-crunfix sd_doc.txt \temp\sd_doc.txt
-crunfix demo.txt \temp\demo.txt
-crunfix sessions.txt \temp\sessions.txt
-crunfix relnotes.txt \temp\relnotes.txt
-crunfix relnotes98.txt \temp\relnotes98.txt
-crunfix relnotes03.txt \temp\relnotes03.txt
-crunfix appnote1.txt \temp\appnote1.txt
-crunfix appnote2.txt \temp\appnote2.txt
-crunfix appnote3.txt \temp\appnote3.txt
-crunfix appnote4.txt \temp\appnote4.txt
-crunfix oldnotes \temp\oldnotes
-pushd \temp
+crunfix sd_doc.txt %TEMP%\sd_doc.txt
+crunfix demo.txt %TEMP%\demo.txt
+crunfix sessions.txt %TEMP%\sessions.txt
+crunfix relnotes.txt %TEMP%\relnotes.txt
+crunfix relnotes98.txt %TEMP%\relnotes98.txt
+crunfix relnotes03.txt %TEMP%\relnotes03.txt
+crunfix appnote1.txt %TEMP%\appnote1.txt
+crunfix appnote2.txt %TEMP%\appnote2.txt
+crunfix appnote3.txt %TEMP%\appnote3.txt
+crunfix appnote4.txt %TEMP%\appnote4.txt
+crunfix oldnotes %TEMP%\oldnotes
+pushd %TEMP%
 tar cvf textdoc.tar sd_doc.txt demo.txt sessions.txt relnotes.txt relnotes98.txt relnotes03.txt appnote1.txt appnote2.txt appnote3.txt appnote4.txt oldnotes
 popd
-copy \temp\textdoc.tar .
+copy %TEMP%\textdoc.tar .
 gzip textdoc.tar -c > textdoc.tar.gz
 del textdoc.tar
--- doc/do_ps.cmd
+++ doc/do_ps.cmd
@@ -1,19 +1,19 @@
 mingw32-make ps.all
 del psdoc.zip psdoc.tar psdoc.tar.gz
 "C:\program files\winzip\wzzip" psdoc sd_doc.ps demo.ps sessions.ps relnotes.ps relnotes98.ps relnotes03.ps appnote1.ps appnote2.ps appnote3.ps appnote4.ps
-crunfix sd_doc.ps \temp\sd_doc.ps
-crunfix demo.ps \temp\demo.ps
-crunfix sessions.ps \temp\sessions.ps
-crunfix relnotes.ps \temp\relnotes.ps
-crunfix relnotes98.ps \temp\relnotes98.ps
-crunfix relnotes03.ps \temp\relnotes03.ps
-crunfix appnote1.ps \temp\appnote1.ps
-crunfix appnote2.ps \temp\appnote2.ps
-crunfix appnote3.ps \temp\appnote3.ps
-crunfix appnote4.ps \temp\appnote4.ps
-pushd \temp
+crunfix sd_doc.ps %TEMP%\sd_doc.ps
+crunfix demo.ps %TEMP%\demo.ps
+crunfix sessions.ps %TEMP%\sessions.ps
+crunfix relnotes.ps %TEMP%\relnotes.ps
+crunfix relnotes98.ps %TEMP%\relnotes98.ps
+crunfix relnotes03.ps %TEMP%\relnotes03.ps
+crunfix appnote1.ps %TEMP%\appnote1.ps
+crunfix appnote2.ps %TEMP%\appnote2.ps
+crunfix appnote3.ps %TEMP%\appnote3.ps
+crunfix appnote4.ps %TEMP%\appnote4.ps
+pushd %TEMP%
 tar cvf psdoc.tar sd_doc.ps demo.ps sessions.ps relnotes.ps relnotes98.ps relnotes03.ps appnote1.ps appnote2.ps appnote3.ps appnote4.ps
 popd
-copy \temp\psdoc.tar .
+copy %TEMP%\psdoc.tar .
 gzip psdoc.tar -c > psdoc.tar.gz
 del psdoc.tar
