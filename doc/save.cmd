186e81a1a8fb1879970e12fba086f17e84f68c34
Robert French <rfrench@seti.org>
2025-05-28 10:35:10 -0700

    Version v36.00

--- doc/save.cmd
+++ doc/save.cmd
@@ -1,1 +1,1 @@
-zip doc%1 *.txi *.htmli makefile *.cmd *.bat *.c *.lst howtobuild texi2html intro.html index.html readings.html download-pc.html download-unix.html getacro.gif
+zip doc%1 *.txi *.htmli makefile *.cmd *.bat *.c *.lst howtobuild COPYING.txt texi2html intro.html index.html readings.html download-pc.html download-unix.html getacro.gif
